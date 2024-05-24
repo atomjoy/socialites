5ed2258354d4c74095c18888a8bd33311ae0c2ae
Atomjoy <atomjoy.official@gmail.com>
2024-05-24 20:41:41 +0200

    Vue3 OneTap vue.blade.php

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Laravel Socialites</title>
+	<title>Laravel Socialites Vue3 Example</title>
 
 	<meta name="description" content="Google and github Login with Laravel Socialite.">
 	<meta name="keywords" content="laravel, socialite">
